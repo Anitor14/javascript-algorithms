6961d36f0e963f6749104e9370805573cc90874d
Anitor <anitorabraham@gmail.com>
2023-01-23 15:44:19 +0100

    worked on queue

---
+++ queue.js
@@ -0,0 +1,62 @@
+function Queue() {
+  collection = ["race", "begotten"];
+  this.print = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (element) {
+    collection.push(element);
+  };
+  this.dequeue = function () {
+    return collection.shift();
+  };
+  this.front = function () {
+    return collection[0];
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
+var q = new Queue();
+q.print();
+q.size();
+
+function PriorityQueue() {
+  var collection = [];
+  this.printCollection = function () {
+    console.log(collection);
+  };
+  this.enqueue = function (element) {
+    if (this.isEmpty()) {
+      collection.push(element);
+    } else {
+      var added = false;
+      for (i = 0; i < collection.length; i++) {
+        if (element < collection[i][1]) {
+          // check priorities
+          collection.splice(i, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        collection.push(element);
+      }
+    }
+  };
+  this.dequeue = function () {
+    var value = collection.shift();
+    return value[0];
+  };
+  this.front = function () {
+    return collection[0];
+  };
+  this.size = function () {
+    return collection.length;
+  };
+  this.isEmpty = function () {
+    return collection.length === 0;
+  };
+}
